08c4cd1a32c2bb628d49c7e2610f4e330e319079
LuizRossini <luiz.guirossini123@gmail.com>
2024-09-09 19:01:55 -0300

    replacing for to lambad expression

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -6,4 +6,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") double avaliacao) {
+
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -31,12 +31,15 @@ public class Principal {
 		}
 		temporadas.forEach(System.out::println);
 
-        for (int i = 0; i < dados.totalTemporadas(); i++) {
+        /*for (int i = 0; i < dados.totalTemporadas(); i++) {
             List<DadosEpisodios> episodiosTemporadas = temporadas.get(i).episodios();
 
             for (int j = 0; j < episodiosTemporadas.size(); j++) {
                 System.out.println(episodiosTemporadas.get(j).titulo());
             }
-        }
+        }*/
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
     }
 }
